f03a872143d7a53dc95ff0688b85fe48a7a0d061
Mario Haim <mario@haim.it>
2022-07-18 14:05:17 +0200

    enhance config and prettify templates

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
-from requests import get, post
+from requests import get, post, ConnectionError
+from requests.models import MissingSchema
 from quart import Quart, render_template, request
 
 app = Quart(__name__)
@@ -7,18 +8,35 @@ app = Quart(__name__)
 
 @app.route("/")
 async def home():
-    return await render_template("home.html.jinja")
+    return await render_template("home.html.jinja",
+                                 error=False,
+                                 error_title="",
+                                 error_message="")
 
 
 @app.route("/setup", methods=["GET", "POST"])
 async def setup():
     if request.method == "GET":
         url = request.args.get('url')
-        osd2f_status_request = get(url + "survey")
+        try:
+            osd2f_status_request = get(url + "survey")
+        except MissingSchema:
+            return await render_template("home.html.jinja",
+                                         error=True,
+                                         error_title="URL missing",
+                                         error_message="Enter a base URL first.")
+        except ConnectionError:
+            return await render_template("home.html.jinja",
+                                         error=True,
+                                         error_title="OSD2F not found",
+                                         error_message="Make sure the URL is correct, that it is only the base URL (e.g., https://data-donation.my-domain.de/), and that OSD2F runs in 'Survey' mode.")
+
         return await render_template("setup.html.jinja",
                                      osd2f=json.loads(osd2f_status_request.text),
+                                     osd2f_prettified=json.dumps(json.loads(osd2f_status_request.text), indent=4),
                                      url=url,
-                                     done=False)
+                                     successfully_done=False,
+                                     defaults={})
     else:
         data = await request.form
         osd2f_setup = post(data['url'] + "survey",
@@ -30,40 +48,45 @@ async def setup():
                                "content": {
                                    "blocks": [],
                                    "upload_box": {
-                                       "explanation": [],
-                                       "header": "hier steht die überschrift"
+                                       "explanation": data['upload_box_explanation'].split('\n'),
+                                       "header": data['upload_box_header']
                                    },
                                    "donate_button": data['donate_button'],
-                                   "empty_selection": "nix drin",
-                                   "file_indicator_text": "",
-                                   "inspect_button": "",
+                                   "empty_selection": data['empty_selection'],
+                                   "file_indicator_text": data['file_indicator_text'],
+                                   "inspect_button": data['inspect_button'],
                                    "preview_component": {
-                                       "entries_in_file_text": "",
-                                       "explanation": [],
-                                       "next_file_button": "",
-                                       "previous_file_button": "vorherige datei",
-                                       "remove_rows_button": "löschen",
-                                       "search_box_placeholder": "",
-                                       "search_prompt": "",
-                                       "title": ""
+                                       "entries_in_file_text": data['preview_component_entries_in_file_text'],
+                                       "explanation": data['preview_component_explanation'].split('\n'),
+                                       "next_file_button": data['preview_component_next_file_button'],
+                                       "previous_file_button": data['preview_component_previous_file_button'],
+                                       "remove_rows_button": data['preview_component_remove_rows_button'],
+                                       "search_box_placeholder": data['preview_component_search_box_placeholder'],
+                                       "search_prompt": data['preview_component_search_prompt'],
+                                       "title": data['preview_component_title']
                                    },
                                    "consent_popup": {
-                                       "accept_button": data['accept_button'],
-                                       "decline_button": data['decline_button'],
-                                       "end_text": "",
-                                       "lead": "",
-                                       "points": [],
-                                       "title": ""
+                                       "accept_button": data['consent_popup_accept_button'],
+                                       "decline_button": data['consent_popup_decline_button'],
+                                       "end_text": data['consent_popup_end_text'],
+                                       "lead": data['consent_popup_lead'],
+                                       "points": data['consent_popup_points'].split('\n'),
+                                       "title": data['consent_popup_title']
                                    },
-                                   "processing_text": "",
-                                   "thanks_text": ""
+                                   "processing_text": data['processing_text'],
+                                   "thanks_text": data['thanks_text']
                                }
                            })
-        with open('osd2f_config.json', 'w') as file:
-            file.write(osd2f_setup.text)
+        osd2f_setup_json = json.loads(osd2f_setup.text)
+        if osd2f_setup_json['success']:
+            with open('osd2f_config.json', 'w') as file:
+                file.write(osd2f_setup.text)
         return await render_template("setup.html.jinja",
-                                     osd2f=json.loads(osd2f_setup.text),
-                                     done=True)
+                                     osd2f=osd2f_setup_json,
+                                     osd2f_prettified=json.dumps(osd2f_setup_json, indent=4),
+                                     url=data['url'],
+                                     successfully_done=osd2f_setup_json['success'],
+                                     defaults=data)
 
 
 @app.route("/survey")
